464b03e4dd1aaf9a353d6dee0f0775dcef27b6f8
EvanCharlesHolloway <Evan.holloway@keyin.com>
2024-03-21 19:42:00 -0230

    work

--- src/main/java/com/keyin/rest/player/PlayerController.java
+++ src/main/java/com/keyin/rest/player/PlayerController.java
@@ -7,27 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController
-@CrossOrigin
-public class PlayerController {
+@RestController @CrossOrigin public class PlayerController {
 
-    @Autowired
-    private PlayerService playerService;
+    @Autowired private PlayerService playerService;
 
     @GetMapping("/player") // Endpoint to retrieve all players
-    public List<Player> getAllPlayers() {
-        return playerService.getAllPlayers();
-    }
+    public List<Player> getAllPlayers() { return playerService.getAllPlayers(); }
 
     @GetMapping("/player/{id}") // Endpoint to retrieve a player by ID
-    public Player getPlayerById(@PathVariable long id) {
-        return playerService.getPlayerById(id);
-    }
+    public Player getPlayerById(@PathVariable long id) { return playerService.getPlayerById(id); }
 
     @PostMapping("/player") // Endpoint to create a new player
-    public Player createPlayer(@RequestBody Player player) {
-        return playerService.createPlayer(player);
-    }
+    public Player createPlayer(@RequestBody Player player) { return playerService.createPlayer(player); }
 
     @PutMapping("/player/{id}") // Endpoint to update an existing player
     public ResponseEntity<Player> updatePlayer(@PathVariable long id, @RequestBody Player player) {
@@ -35,7 +26,5 @@ public class PlayerController {
     }
 
     @DeleteMapping("/player/{id}") // Endpoint to delete a player by ID
-    public void deletePlayerById(@PathVariable long id) {
-        playerService.deletePlayerById(id);
-    }
+    public void deletePlayerById(@PathVariable long id) { playerService.deletePlayerById(id); }
 }
--- src/main/java/com/keyin/rest/player/PlayerService.java
+++ src/main/java/com/keyin/rest/player/PlayerService.java
@@ -4,29 +4,22 @@ import com.keyin.rest.division.Division;
 import com.keyin.rest.division.DivisionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PlayerService {
-    @Autowired private PlayerRepository playerRepository;
+    @Autowired
+    private PlayerRepository playerRepository;
 
-    public List<Player> getAllPlayers() {
-        return (List<Player>) playerRepository.findAll();
-    }
+    public List<Player> getAllPlayers() { return (List<Player>) playerRepository.findAll(); }
 
-    public Player getPlayerById(long id) {
-        return playerRepository.findById(id)
-                .orElseGet(() -> null); // Change made here
-    }
+    public Player getPlayerById(long id) { return playerRepository.findById(id).orElse(null); }
 
-    public void deletePlayerById(long id) {
-        playerRepository.deleteById(id);
-    }
+    public void deletePlayerById(long id) { playerRepository.deleteById(id); }
 
-    public Player createPlayer(Player newPlayer) {
-        return playerRepository.save(newPlayer);
-    }
+    public Player createPlayer(Player newPlayer) { return playerRepository.save(newPlayer); }
 
     public Player updatePlayer(long id, Player updatedPlayer) {
         return playerRepository.findById(id)
@@ -36,7 +29,6 @@ public class PlayerService {
                     existingPlayer.setBirthday(updatedPlayer.getBirthday());
                     return playerRepository.save(existingPlayer);
                 })
-                .orElseGet(() -> null); //
+                .orElse(null);
     }
 }
-
