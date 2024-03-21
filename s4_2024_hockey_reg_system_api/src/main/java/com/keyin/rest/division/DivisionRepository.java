c590e436fd45d3abfc5f85b48fe16ea6ee536127
Tanner Jones <127117143+TannerJ0nes@users.noreply.github.com>
2024-03-21 12:58:38 -0230

    Delete s4_2024_hockey_reg_system_api directory

--- s4_2024_hockey_reg_system_api/src/main/java/com/keyin/rest/division/DivisionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.keyin.rest.division;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DivisionRepository extends CrudRepository<Division, Long> {
-
-}
