faa41be75da33b9856b151837a94eb71b6931fc1
Dawson <127239947+Dawson02@users.noreply.github.com>
2024-03-21 13:37:57 -0230

    Add files via upload

--- src/main/java/com/keyin/rest/player/PlayerController.java
+++ src/main/java/com/keyin/rest/player/PlayerController.java
@@ -9,33 +9,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@RequestMapping("/player")
 public class PlayerController {
+
     @Autowired
     private PlayerService playerService;
 
-    @GetMapping("/player")
+    @GetMapping
     public List<Player> getAllPlayers() {
         return playerService.getAllPlayers();
     }
 
-    @GetMapping("/player/{id}")
-    public Player getPlayerById(@PathVariable long id) {
+    @GetMapping("/{id}")
+    public Player getPlayerById(@PathVariable("id") Long id) {
         return playerService.getPlayerById(id);
     }
 
-    @PostMapping("/player")
-    public Player createPlayer(@RequestBody Player player) {
-        return playerService.createPlayer(player);
+    @PostMapping
+    public Player addPlayer(@RequestBody Player player) {
+        return playerService.addPlayer(player);
     }
 
-    @PutMapping("/player/{id}")
-    public ResponseEntity<Player> updatePlayer(@PathVariable long id, @RequestBody Player player) {
-        return ResponseEntity.ok(playerService.updatePlayer(id, player));
+    @PutMapping("/{id}")
+    public Player updatePlayer(@PathVariable("id") Long id, @RequestBody Player player) {
+        return playerService.updatePlayer(id, player);
     }
 
-    @DeleteMapping("/player/{id}")
-    public void deletePlayerById(@PathVariable long id) {
-        playerService.deletePlayerById(id);
+    @DeleteMapping("/{id}")
+    public void deletePlayer(@PathVariable("id") Long id) {
+        playerService.deletePlayer(id);
     }
 }
--- src/main/java/com/keyin/rest/player/PlayerService.java
+++ src/main/java/com/keyin/rest/player/PlayerService.java
@@ -10,40 +10,31 @@ import java.util.Optional;
 
 @Service
 public class PlayerService {
+
     @Autowired
     private PlayerRepository playerRepository;
 
     public List<Player> getAllPlayers() {
-        return (List<Player>) playerRepository.findAll();
+        return playerRepository.findAll();
     }
 
-    public Player getPlayerById(long id) {
-        Optional<Player> divisionOptional = playerRepository.findById(id);
-
-        return divisionOptional.orElse(null);
+    public Player getPlayerById(Long id) {
+        return playerRepository.findById(id).orElse(null);
     }
 
-    public void deletePlayerById(long id) {
-        playerRepository.deleteById(id);
+    public Player addPlayer(Player player) {
+        return playerRepository.save(player);
     }
 
-    public Player createPlayer(Player newPlayer) {
-        return playerRepository.save(newPlayer);
-    }
-
-    public Player updatePlayer(long id, Player updatedPlayer) {
-        Optional<Player> playerToUpdateOptional = playerRepository.findById(id);
-
-        if (playerToUpdateOptional.isPresent()) {
-            Player playerToUpdate = playerToUpdateOptional.get();
-
-            playerToUpdate.setFirstName(updatedPlayer.getFirstName());
-            playerToUpdate.setLastName(updatedPlayer.getLastName());
-            playerToUpdate.setBirthday(updatedPlayer.getBirthday());
-
-            return playerRepository.save(playerToUpdate);
+    public Player updatePlayer(Long id, Player player) {
+        if (playerRepository.existsById(id)) {
+            player.setId(id);
+            return playerRepository.save(player);
         }
-
         return null;
+    }
+
+    public void deletePlayer(Long id) {
+        playerRepository.deleteById(id);
     }
 }
