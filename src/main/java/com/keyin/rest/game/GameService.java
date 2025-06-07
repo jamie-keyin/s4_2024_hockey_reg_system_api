9b9f46aec4fc7abd0f43ba044099ebbd251f660d
Christian <christian_rose@live.ca>
2025-06-06 21:39:18 -0230

    Add GameService with methods to get and create games

---
+++ src/main/java/com/keyin/rest/game/GameService.java
@@ -0,0 +1,23 @@
+package com.keyin.rest.game;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+    public Game createGame(Game game) {
+        return gameRepository.save(game);
+    }
+}
