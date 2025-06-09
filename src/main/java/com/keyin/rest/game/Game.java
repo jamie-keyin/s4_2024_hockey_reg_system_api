38df0ba90038672f12c33c854bda921bf3f86178
Vanessa Maher <vanessa.maher@keyin.com>
2025-06-09 12:51:36 -0230

    Created Game.java, initial commit

---
+++ src/main/java/com/keyin/rest/game/Game.java
@@ -0,0 +1,42 @@
+package com.keyin.rest.game;
+
+import java.time.LocalDate;
+
+public class Game {
+    private Long id;
+    private String homeTeam;
+    private String awayTeam;
+    private String location;
+    private LocalDate scheduledDate;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public LocalDate getScheduledDate() {
+        return scheduledDate;
+    }
+    public void setScheduledDate(LocalDate scheduledDate) {
+        this.scheduledDate = scheduledDate;
+    }
+}
