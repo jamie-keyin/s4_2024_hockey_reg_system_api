f01d9131c92e918302ba2590f84f417a2331a566
Gerogie18 <jennlyver@gmail.com>
2025-06-11 01:08:51 -0230

    Create GameRepository interface

--- src/main/java/com/keyin/rest/game/GameRepository.java
+++ src/main/java/com/keyin/rest/game/GameRepository.java
@@ -1,4 +1,18 @@
 package com.keyin.rest.game;
 
-public interface GameRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.keyin.rest.team.Team;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface GameRepository extends CrudRepository<Game, Long> {
+    List <Game>findByTeam_Home(Team homeTeam);
+    List <Game>findByTeam_Away(Team awayTeam);
+    List<Game> findByTeam_HomeId(Long homeTeamId);
+    List<Game> findByTeam_AwayId(Long awayTeamId);
+    List <Game>findByLocation(String location);
+    List<Game>findByGameDate(LocalDateTime gameDate);
+
 }
