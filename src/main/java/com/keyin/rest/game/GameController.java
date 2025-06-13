431dcc9a3f0d9a162ecc290e3f7eee1ec2211a95
Gerogie18 <jennlyver@gmail.com>
2025-06-13 12:57:56 -0230

    Create GameController
    functionality: POST, PUT, and DELETE.  GET limited to all and search by id

--- src/main/java/com/keyin/rest/game/GameController.java
+++ src/main/java/com/keyin/rest/game/GameController.java
@@ -1,4 +1,48 @@
 package com.keyin.rest.game;
 
+import com.keyin.rest.division.Division;
+import com.keyin.rest.division.DivisionRepository;
+import com.keyin.rest.player.Player;
+import com.keyin.rest.player.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class GameController {
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping("/game")
+    public List<Game> getAllGames() {return  gameService.getAllGames();}
+
+    @GetMapping("/game/{id}")
+    public ResponseEntity<Game> getTeamById(@PathVariable long id) {
+        Game gameToReturn = gameService.getGameById(id);
+
+        if(gameToReturn == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(gameToReturn);
+    }
+
+    @PostMapping("/game")
+    public Game createGame(@RequestBody Game game) {
+        return gameService.createGame(game);
+    }
+
+    @PutMapping("/game/{id}")
+    public ResponseEntity<Game> updateGame(@PathVariable long id, @RequestBody Game game) {
+        return ResponseEntity.ok(gameService.updateGame(id, game));
+    }
+
+    @DeleteMapping("/game/{id}")
+    public void deleteGameById(@PathVariable long id) {gameService.deleteGameByID(id);}
 }
