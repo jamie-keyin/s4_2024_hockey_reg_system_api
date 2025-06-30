c5c5a4f06297145bfa05ef7892dcf84015cbdbc6
morrisonslue <chris@morrisonslue.com>
2025-06-30 00:52:25 -0230

    added game and the GET/POST endpoints

---
+++ src/main/java/com/keyin/controller/GameController.java
@@ -0,0 +1,25 @@
+package com.keyin.controller;
+
+import com.keyin.domain.Game;
+import com.keyin.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+public class GameController {
+    @Autowired
+    private GameRepository gameRepository;
+
+    @GetMapping
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+    @PostMapping
+    public Game createGame(@RequestBody Game game) {
+        return gameRepository.save(game);
+    }
+}
